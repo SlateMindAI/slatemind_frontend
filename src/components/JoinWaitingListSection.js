d9d927c467d35d47831c639af38af302d7c79dd0
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-09-04 00:30:25 -0400

    Update JoinWaitingListSection.js

--- src/components/JoinWaitingListSection.js
+++ src/components/JoinWaitingListSection.js
@@ -10,7 +10,7 @@ const JoinWaitingListSection = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/join-waiting-list', { name, email });
+            const response = await axios.post('https://slatemindai.com/api/join-waiting-list', { name, email });
             if (response.data.success) {
                 setSuccess(true);
                 setError(null);
