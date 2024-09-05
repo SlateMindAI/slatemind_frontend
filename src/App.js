3c6f76356597df57eab87f2a0c1c7cb39899bd90
rohanmathewalex <rohanmathewalex6674@gmail.com>
2024-09-05 02:02:47 -0400

    final ui updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";  // Ensure the path is correct
 import AboutPage from "./pages/AboutPage";  // Ensure the path is correct
-import RoadmapSection from "./pages/RoadmapSection";
+ 
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} /> 
-        {/* <Route path="/roadmap" element={<RoadmapSection />} /> */}
+      
 
       </Routes>
     </Router>
